3cf575ac822bd120d76130c51363577b23391124
Cameron Mckiddie <cameronsmowing@gmail.com>
2021-12-07 12:46:01 +1000

    Added Health Check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/cmcki85/RESTurant-Full-Stack/internal/transport/http"
+)
 
 // App - struct that contains things like pointer to DB connections
 type App struct{}
@@ -8,6 +13,14 @@ type App struct{}
 // Run - sets up the application
 func (app *App) Run() error {
 	fmt.Println("Setting up API")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to setup server")
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cmcki85/RESTurant-Full-Stack
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
