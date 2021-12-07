f3e67146c39efeea5f598855daeb3021880dc354
Cameron Mckiddie <cameronsmowing@gmail.com>
2021-12-07 12:19:35 +1000

    Updated Application Structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// App - struct that contains things like pointer to DB connections
+type App struct{}
+
+// Run - sets up the application
+func (app *App) Run() error {
+	fmt.Println("Setting up API")
+	return nil
+}
+
 func main() {
 	fmt.Println("Hello")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting REST api.")
+		fmt.Println(err)
+	}
 }
