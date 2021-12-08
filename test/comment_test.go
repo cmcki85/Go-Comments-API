67a79bd81f7c89b6eeddf735e1ae837756ee39a8
Cameron Mckiddie <cameronsmowing@gmail.com>
2021-12-08 14:39:12 +1000

    Added Basic Endpoint testing

---
+++ test/comment_test.go
@@ -0,0 +1,30 @@
+//go:build e2e
+// +build e2e
+
+package test
+
+import (
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetComments(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().Get(BASE_URL + "/api/comment")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, 200, resp.StatusCode())
+}
+
+func TestPostComment(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().
+		SetBody(`{"slug": "/", "author": "12345", "body": "Hello World"}`).
+		Post(BASE_URL + "/api/comment")
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, resp.StatusCode())
+}
